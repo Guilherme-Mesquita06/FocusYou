7e73fa350ad9898b64c79cac5e368e10937a7863
Guilherme-S-Mesquita06 <guilherme.mesquita@sptech.school>
2025-11-13 12:50:33 -0300

    Fit : Correção das pastas clonadas de foorma errada

---
+++ webDataViz/src/database/script-tabelas.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE focusYou;
+
+USE focusYou;
+
+CREATE TABLE usuario (
+id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR (50) NOT NULL,
+	email VARCHAR (100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL,
+    peso DECIMAL (4,2) NOT NULL,
+    dataTreino DATE NOT NULL,
+    fotoPerfil VARCHAR (999)
+);
