d935cdef9d5b3018d8975a1baec0bf038d2ac217
Guilherme-S-Mesquita06 <guilherme.mesquita@sptech.school>
2025-11-20 09:26:10 -0300

    Fix : Mudando estrutura HTML da pagina addSeries (front-end)

--- webDataViz/src/models/treinoModel.js
+++ webDataViz/src/models/treinoModel.js
@@ -1,6 +1,7 @@
 var database = require("../database/config");
 
 
+
 function cadastrar(titulo, observacao, idFicha) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", titulo, observacao, idFicha);
     var instrucaoSql = `
